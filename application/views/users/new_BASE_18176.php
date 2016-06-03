61a42311b61a3cdd1bf47169b4527c9f0d253175
Rehnan Carolino <rehnancarolino@live.com>
2016-06-02 22:43:30 -0300

    update user account

---
+++ application/views/users/new_BASE_18176.php
@@ -0,0 +1,10 @@
+<div class="pg pg-cadastro">
+ 	<div class="form-cadastro">
+ 	   <article>
+ 	     <?php echo form_sign_up('account', $new_user, ''); ?>
+ 	     <?= anchor('login', 'Voltar para o Login', 'title="Página de Login"') ?>
+ 	     <br>
+ 	     <?php echo validation_errors(); ?>
+ 		</article>
+ 	</div>
+ </div>
