9eaf7641f5ffbbfbdc0d068133a0c60500b8ba0c
Rehnan Carolino <rehnancarolino@live.com>
2016-06-02 22:46:51 -0300

    conflict resolved

--- application/views/users/new.php
+++ application/views/users/new.php
@@ -1,7 +1,6 @@
 <div class="pg pg-cadastro">
 	<div class="form-cadastro">
 	   <article>
-
 	     <?php echo form_sign_up('account', $new_user, ''); ?>
 	     <?= anchor('login', 'Voltar para o Login', 'title="Página de Login"') ?>
 	     <br>
